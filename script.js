696eb2e89a767a776225efcb62e0a1c2096ea456
SiddheshwarKale <siddkale2005@gmail.com>
2025-08-15 19:27:23 +0530

    javaScript Added

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+"use strict";
 
+const btn = document.querySelector(".btn");
+const output = document.querySelector(".output");
 
+btn.addEventListener("click", () => {
+  const recognition = new webkitSpeechRecognition();
+  recognition.lang = "en-US";
+  recognition.start();
+  recognition.onresult = (event) => {
+    output.textContent = event.results[0][0].transcript;
+  };
+});
